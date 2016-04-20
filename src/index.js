514879226657e38ded89a27fa9b0719b95a10b8b
Ranadeep Polavarapu <ranadeep@icloud.com>
2016-04-19 20:34:26 -0400

    finished boilerplate from node-sq-api

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import events from 'events';
 import util from 'util';
 import LineInputStream from 'line-input-stream';
 
-import TS_SERVERQUERY_COMMANDS from './commands';
+import {
+    TS_SERVERQUERY_COMMANDS as TS_SQ_COMMANDS
+} from './commands';
 
 const TS3_SQ_SERVER_HOST = process.env.TS3_SQ_SERVER_HOST || 'localhost';
 const TS3_SQ_SERVER_PORT = process.env.TS3_SQ_SERVER_PORT || 10011;
@@ -22,8 +24,9 @@ export default class TeamSpeakSQClient {
         self.reader = null;
         self.queue = [];
         self.executing = null;
+        self.api = {};
 
-        self.initCmds();
+        self._initCmds();
 
         self.connect();
 
@@ -48,33 +51,6 @@ export default class TeamSpeakSQClient {
         });
 
         self._onConnect();
-
-        return self;
-    }
-
-    _onConnect() {
-        var self = this;
-
-        self.socket.on('connect', () => {
-            self.reader = LineInputStream(self.socket);
-
-            self.reader.on('line', data => {
-                var readerDataStr = data.trim();
-
-                if (self.status < 0) {
-                    self.status++;
-                    if (self.status === 0) {
-                        self.advanceQueue();
-                    }
-
-                    return;
-                }
-
-                self._onData(readerDataStr);
-            });
-
-            self.emit('connect');
-        });
 
         return self;
     }
@@ -180,6 +156,181 @@ export default class TeamSpeakSQClient {
 
         return queue;
     }
+
+    _initCmds() {
+        var self = this;
+
+        TS_SQ_COMMANDS.forEach(cmd => {
+            self.api[cmd] = (() => {
+                var args = Array.prototype.slice.call(arguments);
+
+                args.unshift(cmd);
+
+                return self.send.apply(self, args);
+            });
+        });
+
+        return self;
+    }
+
+    _onConnect() {
+        var self = this;
+
+        self.socket.on('connect', () => {
+            self.reader = LineInputStream(self.socket);
+
+            self.reader.on('line', data => {
+                var readerDataStr = data.trim();
+
+                if (self.status < 0) {
+                    self.status++;
+                    if (self.status === 0) {
+                        self.advanceQueue();
+                    }
+
+                    return;
+                }
+
+                self._onData(readerDataStr);
+            });
+
+            self.emit('connect');
+        });
+
+        return self;
+    }
+
+    _onData(dataStr) {
+        var self = this;
+
+        if (dataStr.indexOf('error') === 0) {
+            var resp = parseResp(dataStr.substr(6).trim());
+
+            if (resp.id === 0) {
+                self.executing.error = null;
+                if (!self.executing.resp) {
+                    self.executing.resp = {
+                        status: 'ok',
+                        raw: dataStr,
+                    };
+                }
+            } else {
+                self.executing.error = {
+                    status: 'error',
+                    message: resp.msg,
+                    error_id: resp.id,
+                };
+            }
+
+            var req = {
+                cmd: self.executing.cmd,
+                options: self.executing.options,
+                params: self.executing.parameters,
+                raw: self.executing.text,
+            };
+
+            if (typeof self.executing.cb == 'function') {
+                self.executing.cb.call(
+                    self.executing,
+                    self.executing.error,
+                    self.executing.resp,
+                    req
+                );
+            } else {
+                self.emit(
+                    self.executing.cmd,
+                    self.executing.error,
+                    self.executing.resp,
+                    req
+                );
+            }
+
+            self.executing = null;
+            self.advanceQueue();
+        } else if (dataStr.indexOf('notify') === 0) {
+            dataStr = dataStr.substr(6);
+
+            var eventName = dataStr.substr(0, dataStr.indexOf(' '));
+            var notifyResp = parseResp(dataStr.substr(eventName.length + 1));
+
+            self.emit('notify', eventName, notifyResp);
+
+            self.emit('notify.' + eventName, eventName, notifyResp);
+        } else if (self.executing) {
+            self.executing.resp = {
+                status: 'ok',
+                data: parseResp(dataStr),
+                raw: dataStr,
+            };
+        }
+
+        return self;
+    }
+
+    escape(str) {
+        str = str.replace(/\\/g, '\\\\');
+        str = str.replace(/\//g, '\\/');
+        str = str.replace(/\|/g, '\\p');
+        str = str.replace(/\n/g, '\\n');
+        str = str.replace(/\r/g, '\\r');
+        str = str.replace(/\t/g, '\\t');
+        str = str.replace(/\v/g, '\\v');
+        str = str.replace(/\f/g, '\\f');
+        str = str.replace(/ /g, '\\s');
+
+        return str;
+    }
+
+    unescape(str) {
+        str = str.replace(/\\s/g, ' ');
+        str = str.replace(/\\p/g, '|');
+        str = str.replace(/\\n/g, '\n');
+        str = str.replace(/\\f/g, '\f');
+        str = str.replace(/\\r/g, '\r');
+        str = str.replace(/\\t/g, '\t');
+        str = str.replace(/\\v/g, '\v');
+        str = str.replace(/\\\//g, '\/');
+        str = str.replace(/\\\\/g, '\\');
+
+        return str;
+    }
+
+    parseResp(str) {
+        var self = this;
+        var resp = [];
+        var records = str.split('|');
+
+        resp = records.map(k => {
+            var args = k.split(' ');
+            var obj = {};
+
+            args.forEach(v => {
+                if (v.indexOf('=') > -1) {
+                    var key = unescape(v.substr(0, v.indexOf('=')));
+                    var value = unescape(v.substr(v.indexOf('=') + 1));
+
+                    if (parseInt(value, 10) == value) {
+                        value = parseInt(value, 10);
+                    }
+
+                    obj[key] = value;
+
+                } else {
+                    obj[v] = '';
+                }
+            });
+
+            return obj;
+        });
+
+        if (resp.length === 0) {
+            resp = null;
+        } else if (resp.length === 1) {
+            resp = resp.shift();
+        }
+
+        return resp;
+    };
 }
 
 util.inherits(TeamSpeakSQClient, events.EventEmitter);
